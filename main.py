bc456bdf6255e273d9a4297c697662d5a43c86c6
MarianeCaliari <107563524+MarianeCaliari@users.noreply.github.com>
2025-04-27 00:50:37 -0300

    ok

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-
 @app.get("/funcaoteste")
 async def funcaoteste():
     return {"teste": True, "num_aleatorio":  random.randint (0, 20000)}
