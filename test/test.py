8654c4f058bd5ad717e6b4a3d55dbe06cda347dc
MarianeCaliari <107563524+MarianeCaliari@users.noreply.github.com>
2025-04-27 00:19:39 -0300

    atualizacao3-test

--- test/test.py
+++ test/test.py
@@ -3,13 +3,16 @@ from unittest.mock import patch
 
 
 def test_root():
-    assert root() == {"message": "Hello World"}
+    result = root()
+    yield result
+    assert result == {"message": "Hello World"}
 
 
 
 def funcaoteste():
     with patch('random.randint', return_value=12345):
         result = funcaoteste()
+        yield result
 
     assert result == {"teste": True, "num_aleatorio": 12345}
 
